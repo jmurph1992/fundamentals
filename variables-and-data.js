f15932fe85646cacfbcbcb96382ef0fa0e9139cb
Justin Murphy <j.murphyjt@gmail.com>
2019-06-26 14:53:04 -0700

    JS fundamentals upload

---
+++ variables-and-data.js
@@ -0,0 +1,18 @@
+/*
+*Variables, Data Types, and Typing
+ */
+
+let wordData = "Word Data";
+
+console.log(wordData);
+
+let obj = {
+	key1: "value 1",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2Key1: "Internal Object Value",
+	},
+};
+
+console.log(obj.obj2.obj2Key1);
